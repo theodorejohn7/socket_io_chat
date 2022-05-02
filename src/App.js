c10247f51e208d7415d4e878a75b75cd17c0730c
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-05-02 19:04:08 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+
+  const joinRoom = () => {
+    if ((username !== "") & (room !== "")) {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      {
+        !showChat ?
+      (
+      <div className="joinChatContainer">
+        <h3>Welcome</h3>
+
+        <input
+          type="text"
+          placeholder="Key-in Your Name"
+          onChange={(events) => {
+            setUsername(events.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Key-in Room ID"
+          onChange={(events) => {
+            setRoom(events.target.value);
+          }}
+        />
+        <button onClick={joinRoom}> Join the Chat room </button>
+        </div>)
+:
+(        <Chat  socket={socket} username={username} room={room} />)
+        }
+        </div>
+
   );
 }
 
---
+++ src/Chat.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        sender: username,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
+    }
+  };
+
+  // useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+      // console.log(data);
+    });
+  // }, [socket]);
+
+  return (
+    <div className="chat-window">
+      {/* <div className="chat-header">
+        <p>You arein Chat Room </p>
+      </div> */}
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.sender ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p id="inner"  >  </p>
+                    <p id="author">{messageContent.sender}</p>
+
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currentMessage}
+          placeholder="Key-in a message"
+          onChange={(events) => setCurrentMessage(events.target.value)}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+
+<Stack spacing={2} direction="row">
+      <Button variant="contained" onClick={sendMessage}>Send</Button>
+
+      </Stack>
+        {/* <button style={{backgroundColor:"blue"}} onClick={sendMessage}>Send</button> */}
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
